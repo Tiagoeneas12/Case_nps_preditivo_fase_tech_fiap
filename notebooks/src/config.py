01f361eb83e87e4c1be8a58b1058b80cc85678b8
Tiago Eneas <tiagoeneas70@gmail.com>
2026-09-21 22:37:27 -0300

    Add files via upload

---
+++ notebooks/src/config.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+PROJECT_FOLDER = Path(__file__).resolve().parents[2]
+
+DATA_FOLDER = PROJECT_FOLDER / "data"
+
+# Path to the project's data files
+RAW_DATA = DATA_FOLDER / "raw" / "desafio_nps_fase_1.csv"
+CLEAN_DATA = DATA_FOLDER / "processed" /  "base_nps.parquet"
+
+# Path to the project's model files
+MODELS_FOLDER = PROJECT_FOLDER / "models"
+
+# Other project paths
+REPORTS_FOLDER = PROJECT_FOLDER / "reports"
+IMAGES_FOLDER = REPORTS_FOLDER / "images"
